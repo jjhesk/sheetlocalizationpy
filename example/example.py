fc2f389fd4b0276e68aad42cff91abe7bebcbd05
heskdx <jobhesk@gmail.com>
2020-11-03 18:21:10 +0800

    package updates related items. please check in commit details

--- example/example.py
+++ example/example.py
@@ -5,9 +5,10 @@ import os
 from googlesheettranslate.main import GoogleTranslationSheet
 
 ROOT = os.path.join(os.path.dirname(__file__))
-
+sheetID = "fjosijfijsoeif"
+gid = "fjsoaiejfoijseoifjo"
 builder = GoogleTranslationSheet().builderOutputTarget(ROOT).builderMeta(
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vR9-Nx_JTxmBP9rRTfGapdWWB2CQ4EBDBHwS8ZbIMg_6_yZcaWE7gVMs4vLd8npOnEUjJhpMnE3cPCS/pubhtml?gid=1872832543&single=true"
+    "https://docs.google.com/spreadsheets/d/e/{}/pubhtml?gid={}&single=true".format(sheetID, gid)
 )
 # builder.GetReader().overrideFileFormat("_{}.json", True)
 builder.builderTransformers("Android")
