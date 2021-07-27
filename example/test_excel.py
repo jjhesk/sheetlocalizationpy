cea76d4182b6ba2226261e1dbb1e9410d5d1954a
heskdx <jobhesk@gmail.com>
2021-07-27 13:33:52 +0800

    package updates related items. please check in commit details

--- example/test_excel.py
+++ example/test_excel.py
@@ -3,7 +3,7 @@ import os
 from googlesheettranslate.main import GoogleTranslationSheet
 
 ROOT = os.path.join(os.path.dirname(__file__))
-builder = GoogleTranslationSheet().builderOutputTarget(ROOT).builderFromExcel("emp-translation.xlsx").builderTransformers("i18n")
+builder = GoogleTranslationSheet().builderOutputTarget(ROOT).builderFromExcel("emp-translation.xlsx").builderTransformers("py")
 builder.GetReader().overrideFileFormat("__i{}.json", True)
 builder.run("EN")
 builder.run("ZH")
